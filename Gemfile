0795b54138b790178157a2ea99781be16088f937
Scott Crawford <scottcrawford03@gmail.com>
2015-12-05 22:33:56 -0500

    add users and current user

--- Gemfile
+++ Gemfile
@@ -9,12 +9,13 @@ gem 'coffee-rails', '~> 4.1.0'
 gem 'jquery-rails'
 gem 'jbuilder', '~> 2.0'
 gem 'sdoc', '~> 0.4.0', group: :doc
-# Use ActiveModel has_secure_password
-# gem 'bcrypt', '~> 3.1.7'
+gem 'bcrypt', '~> 3.1.7'
 
 group :development, :test do
   gem 'rspec-rails'
   gem 'byebug'
   gem 'web-console', '~> 2.0'
   gem 'spring'
+  gem 'pry-rails'
+  gem 'pry'
 end
---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,22 @@
+class SessionsController < ApplicationController
+  def create
+    user = User.find_by(username: permitted_params[:username])
+    if user && user.authenticate(permitted_params[:password])
+      session[:user_id] = user.id
+      redirect_to root_url
+    else
+      redirect_to root_url
+    end
+  end
+
+  def destory
+    session.clear
+    redirect_to root_url
+  end
+
+  private
+
+  def permitted_params
+    params.require(:login).permit(:username, :password)
+  end
+end
